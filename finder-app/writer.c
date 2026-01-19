dfa85d7c95252dcefa5c360955e591512cc7e604
Donald Lamar Davis II <don@ddavis2.com>
2026-01-19 01:41:23 +0000

    Make and writer work on native host

---
+++ finder-app/writer.c
@@ -0,0 +1,47 @@
+#include <unistd.h>
+#include <fcntl.h>
+#include <syslog.h>
+#include <errno.h>
+#include <string.h>
+
+int write_data_to_file(const char *filename, const char *data) {
+    int ret_code = 0;
+    int fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+    if (fd == -1) {
+        syslog(LOG_ERR, "Error opening file for writing: %m");
+        return ret_code = -1;
+    }
+    ssize_t bytes_written = write(fd, data, strlen(data));
+    if (bytes_written == -1) {
+        syslog(LOG_ERR, "Error writing to file: %m");
+        ret_code = -1;
+    }
+    if (close(fd) == -1) {
+        syslog(LOG_ERR, "Error closing file: %m");
+        ret_code = -1;
+    }
+    return ret_code;
+}
+
+int main(int argc, char *argv[]) {
+    int ret_code = 0;
+    openlog("writer", LOG_USER, LOG_USER);
+
+    if (argc < 3) {
+        syslog(LOG_ERR, "Usage: %s <file> <text>", argv[0]);
+        closelog();
+        return ret_code = -1;
+    }
+
+    const char *filename = argv[1];
+    const char *text = argv[2];
+
+    syslog(LOG_INFO, "Writing %s to %s", text, filename);
+    ret_code = write_data_to_file(filename, text);
+    if (ret_code == 0) {
+        syslog(LOG_INFO, "Data written to %s successfully.", filename);
+    }
+
+    closelog();
+    return ret_code;
+}
